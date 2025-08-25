0ef06417fbc9cc459988cec83a903b8198f26109
Yasir Turk <yasirmturk@gmail.com>
2025-08-26 01:52:56 +0200

    Adds initial content

--- src/settings.ts
+++ src/settings.ts
@@ -1,19 +1,19 @@
 export const profile = {
-	fullName: 'Your Name',
-	title: '',
-	institute: '',
-	author_name: '', // Author name to be highlighted in the papers section
+	fullName: 'Hikmat Khan',
+	title: 'Dr.',
+	institute: 'American University of Sharjah',
+	author_name: 'Yasir Turk', // Author name to be highlighted in the papers section
 	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
+		{ title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
 }
 
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
-	email: '',
-	linkedin: '',
+	email: 'dr@hikmatkhan.com',
+	linkedin: 'https://',
 	x: 'https://www.x.com/',
-	github: '',
+	github: 'https://github.com/yasirmturk/hikmatkhan.com',
 	gitlab: '',
 	scholar: '',
 	inspire: '',
@@ -21,7 +21,7 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://localhost:51677', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
@@ -32,7 +32,7 @@ export const template = {
 }
 
 export const seo = {
-	default_title: 'Astro Academia',
-	default_description: 'Astro Academia is a template for academic websites.',
+	default_title: 'Hikmat Khan',
+	default_description: 'Hikmat Khan is a doctor for artificial intelligence.',
 	default_image: '/images/astro-academia.png',
 }
